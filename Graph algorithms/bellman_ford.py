e28766ca3b9b113ce7b03f9d8cda1af324d235c1
Артём <a.burgart02@gmail.com>
2022-05-30 15:49:17 +0500

    Add path restoring to bellman-ford

--- Graph algorithms/bellman_ford.py
+++ Graph algorithms/bellman_ford.py
@@ -6,18 +6,28 @@ class Graph:
     def add_edge(self, v1, v2, weight):
         self.edges.append([v1, v2, weight])
 
-    def bellman_ford(self, start_v):
+    def bellman_ford(self, start_v, target_v):
         D = {v: float('inf') for v in range(self.v)}
+        P = {p: -1 for p in range(self.v)}
         D[start_v] = 0
         for _ in range(self.v - 1):
             for u, v, w in self.edges:
                 if D[u] != float('inf') and D[u] + w < D[v]:
                     D[v] = D[u] + w
+                    P[v] = u
         for u, v, w in self.edges:
             if D[u] != float('inf') and D[u] + w < D[v]:
                 print('В графе есть цикл отрицательного веса')
                 return
-        return D
+        if D[target_v] == float('inf'):
+            print(f'Нет пути от вершины {start_v} до вершины {target_v}')
+        else:
+            path = []
+            while target_v != -1:
+                path.append(target_v)
+                target_v = P[target_v]
+            [print(v, end=' ') for v in reversed(path)]
+        print(D)
 
 
 g = Graph(9)
@@ -36,4 +46,4 @@ g.add_edge(4, 8, 5)
 g.add_edge(5, 8, 12)
 g.add_edge(6, 7, 1)
 g.add_edge(7, 8, 3)
-print(g.bellman_ford(0))
+g.bellman_ford(0, 8)
