c972bf9628aeec5e66e9c1d10f4f13f75c249f9d
Артём <a.burgart02@gmail.com>
2022-06-03 17:29:31 +0500

    Fix trivial prefix function

--- String algorithms/trivial_prefix_function.py
+++ String algorithms/trivial_prefix_function.py
@@ -2,7 +2,7 @@ def trivial_prefix_function(string):
     p = [0] * len(string)
     for i in range(len(string)):
         for k in range(i):
-            if string[:k+1] == string[i-k:i+1] and 2 * (k + 1) <= i + 1:
+            if string[:k+1] == string[i-k:i+1]:
                 p[i] = k + 1
     return p
 
