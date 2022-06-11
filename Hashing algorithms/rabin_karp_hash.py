c499febaf09f3fed4b28509649bd94a8b7c27800
Артём <a.burgart02@gmail.com>
2022-06-11 16:03:57 +0500

    Improve rabin-karp hash

--- Hashing algorithms/rabin_karp_hash.py
+++ Hashing algorithms/rabin_karp_hash.py
@@ -2,13 +2,14 @@ import random
 
 
 p = 2 ** 64 - 59
+x = random.randint(1, p - 1)
 
 
 def rabin_karp_hash(s):
     if len(s) == 1:
         return ord(s[0])
-    x, h = random.randint(1, p - 1), ord(s[0])
-    for k in range(1, len(s)):
+    h = 0
+    for k in range(len(s)):
         h = h * x + ord(s[k])
     return h % p
 
