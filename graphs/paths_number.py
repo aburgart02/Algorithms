6dea4c9d82c1de9ba7e8a6a01154e7331fa71cf4
Артём <a.burgart02@gmail.com>
2022-06-19 12:51:25 +0500

    Improve paths number

--- graphs/paths_number.py
+++ graphs/paths_number.py
@@ -7,19 +7,15 @@ graph = {
     5: [6],
     6: []
 }
-memory = [-1] * len(graph.keys())
 
 
 def count_paths(start_v, target_v):
-    if memory[start_v] != -1:
-        return memory[start_v]
-    if start_v == target_v:
-        return 1
-    result = 0
-    for v in graph[start_v]:
-        result += count_paths(v, target_v)
-    memory[start_v] = result
-    return result
+    dp = [0] * len(graph.keys())
+    dp[target_v] = 1
+    for v in range(target_v - 1, -1, -1):
+        for next in graph[v]:
+            dp[v] += dp[next]
+    return dp[start_v]
 
 
 print(count_paths(0, 6))
