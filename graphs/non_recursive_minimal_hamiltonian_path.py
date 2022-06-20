25735ab8eaf5c937876a7826ba23fc5e5b7af41f
Артём <a.burgart02@gmail.com>
2022-06-20 20:56:12 +0500

    Add path restoring to non recursive minimal hamiltonian path

--- graphs/non_recursive_minimal_hamiltonian_path.py
+++ graphs/non_recursive_minimal_hamiltonian_path.py
@@ -6,7 +6,7 @@ class Graph:
     def __init__(self, v_number):
         self.v = v_number
         self.graph = defaultdict(list)
-        self.dp = [[sys.maxsize for _ in range(self.v)] for _ in range(1 << self.v)]
+        self.dp = [[[sys.maxsize, None] for _ in range(self.v)] for _ in range(1 << self.v)]
 
     def add_edge(self, v1, v2, w):
         self.graph[v1].append((v2, w))
@@ -17,16 +17,26 @@ class Graph:
 
     def minimal_hamiltonian_path(self):
         for v in range(self.v):
-            self.dp[(1 << self.v) - 1][v] = 0
+            self.dp[(1 << self.v) - 1][v][0] = 0
         for used in range((1 << self.v) - 2, -1, -1):
             for v in range(self.v):
                 for to, c in self.graph[v]:
                     if self.get_bit(used, to) == 0:
-                        self.dp[used][v] = min(self.dp[used][v], c + self.dp[used | (1 << to)][to])
-        ans = sys.maxsize
+                        if c + self.dp[used | (1 << to)][to][0] < self.dp[used][v][0]:
+                            self.dp[used][v] = [c + self.dp[used | (1 << to)][to][0], to]
+        length = sys.maxsize
+        start_v = 0
         for v in range(self.v):
-            ans = min(ans, self.dp[1 << v][v])
-        return ans
+            if self.dp[1 << v][v][0] < length:
+                length = self.dp[1 << v][v][0]
+                start_v = v
+        path = [start_v]
+        used = 1 << start_v
+        while used != (1 << self.v) - 1:
+            start_v = self.dp[used][start_v][1]
+            path.append(start_v)
+            used |= 1 << start_v
+        return path, length
 
 
 g = Graph(7)
